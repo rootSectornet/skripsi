58f26001a22cd3272842e9b3fceca9e4afd7e313
donbay28 <donbaysaputra28@gmail.com>
2020-10-16 15:45:22 +0700

    odading mang oleh

--- models/pembelians.js
+++ models/pembelians.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      pembelians.belongsTo(models.stocks, {
+        foreignKey: 'stock_id',
+      })
     }
   };
   pembelians.init({
