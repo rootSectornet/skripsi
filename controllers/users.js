d1053e56ad521a2d90ba1d6e5dbe3d55e72b715c
rootip <tedijammz@gmail.com>
2020-10-17 01:17:33 +0700

    s

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,10 @@
 const users = require('./../models').users
+const rijndael = require('./../controllers/rijndael')
 
 module.exports = {
     async getList(body) {
-        const {email, password} = body
+        let text = rijndael.decrypt(req.body)
+        const {email, password} = text
         return await users.findOne({
             where: {
                 email: email
